00afae0635a548a45dda5744be36f438a3d71242
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2024-05-20 15:56:23 +0530

    Create LowerBound.java

---
+++ LowerBound.java
@@ -0,0 +1,20 @@
+class Solution {
+   // Smallest iindex such that arr[index]>=target
+    public int lowerBOund(int[] nums, int target) {
+        int n =nums.length;
+        int si =0;
+        int ei=n-1;
+      int ans=n;
+        while(si<=ei){
+            int mid =si+(ei-si)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+              ei=mid-1;
+            } else{
+                si=mid+1;
+            } 
+        }
+
+        return ans;
+    }
+}
