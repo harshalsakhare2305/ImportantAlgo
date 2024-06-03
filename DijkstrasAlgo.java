cb196bdcaba3701782fdbece944601e83e695a90
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2024-06-03 12:05:48 +0530

    Update DijkstrasAlgo.java

--- DijkstrasAlgo.java
+++ DijkstrasAlgo.java
@@ -30,3 +30,44 @@
             }
    return distance;
  }
+
+
+
+
+
+
+
+  public int[] DijkstrasAlgo(ArrayList<int[]>[] graph,int src,int V){
+            PriorityQueue<int[]> pq =new PriorityQueue<>(((a,b)->a[0]-b[0]));
+          
+            int[] distance =new int[V+1];
+            for(int i=0;i<=V;i++){
+                if(i!=src){
+                    distance[i]=Integer.MAX_VALUE;
+                }
+
+            }
+            pq.add(new int[]{0,src});
+            
+
+            while (!pq.isEmpty()) {
+                int[] curr =pq.remove();
+                int node =curr[1];
+              
+                    for(int i=0;i<graph[node].size();i++){
+                       int[] e = graph[node].get(i);
+                      
+                      int v =e[0];
+                      int wt =e[1];
+
+                      if(distance[node]+wt <distance[v]){
+                        distance[v]=distance[node]+wt;
+                        pq.add(new int[]{distance[v],v});
+                      }
+                    }
+
+
+                
+            }
+   return distance;
+ }
