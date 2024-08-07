597e92516baaef590b2fbc7c44e3f5c7c4f10da8
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2024-08-07 23:17:37 +0530

    Update GCDAlgo.java

--- GCDAlgo.java
+++ GCDAlgo.java
@@ -1,5 +1,9 @@
 
 //Time Complexity of Eucledian Algorithm is O(log(max(a,b))
+
+//we know if b>a  gcd(a,b)=gcd(a,b-a)
+
+//ultimate is gcd(a,b)=gcd(b%a,a)
 public int gcd(int a,int b){
 if(a==0){
   return b;
